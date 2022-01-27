aae7e2d194c636259e86ab85433563bf129bb38a
Mike Ackerman <dirtpathwanderer@gmail.com>
2022-01-27 15:13:08 -0700

    standardized some data naming conventions

--- analysis/R_scripts/prep_UWW_data.R
+++ analysis/R_scripts/prep_UWW_data.R
@@ -12,18 +12,19 @@ library(tidyverse)
 library(magrittr)
 library(ggmap)
 library(nngeo)
+library(rgdal)
 
 #-----------------------------------------------------------------
 # set NAS prefix, depending on operating system
 if(.Platform$OS.type != 'unix') {nas_prefix = "S:"}
 
 # set default crs
-ww_crs = st_crs(4326) # WGS84
+uww_crs = st_crs(4326) # WGS84
 
 #-----------------------------------------------------------------
 # read in HUC12 watershed boundaries from NAS, majority of PNW
 huc12_sf = st_read(paste0(nas_prefix, "data/habitat/watershed_boundaries/WBDHU12.shp")) %>%
-  st_transform(ww_crs)
+  st_transform(uww_crs)
 
 #-----------------------------------------------------------------
 # read in current QRF extrapolations, for entire CRB
@@ -37,31 +38,31 @@ huc12_sf = st_read(paste0(nas_prefix, "data/habitat/watershed_boundaries/WBDHU12
 # if needed, these .gpkg files can be saved locally to decrease read times, e.g.,
 # on Mike's machine...
 sum_juv_sf = st_read("C:/Workspace/qrf/Rch_Cap_RF_juv_summer_dash.gpkg") %>%
-  st_transform(ww_crs)
+  st_transform(uww_crs)
 win_juv_sf = st_read("C:/Workspace/qrf/Rch_Cap_RF_juv_winter.gpkg") %>%
-  st_transform(ww_crs)
+  st_transform(uww_crs)
 redd_sf = st_read("C:/Workspace/qrf/Rch_Cap_RF_redds.gpkg") %>%
-  st_transform(ww_crs)
+  st_transform(uww_crs)
 # these are too large to reasonably plot
 
 #-----------------------------------------------------------------
 # focus on Walla Walla, and compute total capacity at each reach
 # note that this does NOT filter for species ranges, yet
-ww_huc_sf = st_read(here("analysis/data/raw_data/watershed_boundary/UpperWWAssessmentStudyArea.shp")) %>%
-  st_transform(ww_crs) %>%
+uww_huc_sf = st_read(here("analysis/data/raw_data/watershed_boundary/UpperWWAssessmentStudyArea.shp")) %>%
+  st_transform(uww_crs) %>%
   mutate(HUC8 = str_sub(HUC12, 1, 8),
          HUC10 = str_sub(HUC12, 1, 10))
 
 # plot ww_huc_sf
-ggplot(data = ww_huc_sf) +
+ggplot(data = uww_huc_sf) +
   geom_sf()
 
 #-----------------------------------------------------------------
 # filter WW QRF extrapolations
 
 # summer parr
-ww_sum_sf = sum_juv_sf %>%
-  st_intersection(ww_huc_sf %>%
+uww_sum_sf = sum_juv_sf %>%
+  st_intersection(uww_huc_sf %>%
                     st_union() %>%
                     nngeo::st_remove_holes()) %>%
   # calculate summer, juvenile capacities for each reach
@@ -70,7 +71,7 @@ ww_sum_sf = sum_juv_sf %>%
          sthd_n = sthd_per_m * reach_leng,
          sthd_n_se = sthd_per_m_se * reach_leng) %>%
   # add HUC codes
-  st_join(ww_huc_sf %>%
+  st_join(uww_huc_sf %>%
             select(Name,
                    HUC8,
                    HUC10,
@@ -78,11 +79,11 @@ ww_sum_sf = sum_juv_sf %>%
           join = st_covered_by,
           left = F)
 
-ggplot(ww_sum_sf) + geom_sf()
+ggplot(uww_sum_sf) + geom_sf()
 
 # winter presmolt
-ww_win_sf = win_juv_sf %>%
-  st_intersection(ww_huc_sf %>%
+uww_win_sf = win_juv_sf %>%
+  st_intersection(uww_huc_sf %>%
                     st_union() %>%
                     nngeo::st_remove_holes()) %>%
   # calculate winter, juvenile capacities for each reach
@@ -91,7 +92,7 @@ ww_win_sf = win_juv_sf %>%
          sthd_n = sthd_per_m * reach_leng,
          sthd_n_se = sthd_per_m_se * reach_leng) %>%
   # add HUC codes
-  st_join(ww_huc_sf %>%
+  st_join(uww_huc_sf %>%
             select(Name,
                    HUC8,
                    HUC10,
@@ -99,11 +100,11 @@ ww_win_sf = win_juv_sf %>%
           join = st_covered_by,
           left = F)
 
-ggplot(ww_win_sf) + geom_sf()
+ggplot(uww_win_sf) + geom_sf()
 
 # redds
-ww_redd_sf = redd_sf %>%
-  st_intersection(ww_huc_sf %>%
+uww_redd_sf = redd_sf %>%
+  st_intersection(uww_huc_sf %>%
                     st_union() %>%
                     nngeo::st_remove_holes()) %>%
   # calculate redd capacities for each reach
@@ -112,7 +113,7 @@ ww_redd_sf = redd_sf %>%
          sthd_n = sthd_per_m * reach_leng,
          sthd_n_se = sthd_per_m_se * reach_leng) %>%
   # add HUC codes
-  st_join(ww_huc_sf %>%
+  st_join(uww_huc_sf %>%
             select(Name,
                    HUC8,
                    HUC10,
@@ -120,74 +121,75 @@ ww_redd_sf = redd_sf %>%
           join = st_covered_by,
           left = F)
 
-ggplot(ww_redd_sf) + geom_sf()
+ggplot(uww_redd_sf) + geom_sf()
 
 #-----------------------------------------------------------------
-# get 200m reach layer for this area
+# get 200m reach layer for this area; contains a bunch of habitat metrics for the 200m reaches
 # this dataset is in the QRFcapacity repo
 load("C:/Git/QRFcapacity/data/rch_200.rda")
 
-# alternative by installing QRFcapacity R package...
+# alternatively, by installing QRFcapacity R package...
 # library(devtools)
 # devtools::install_github("KevinSee/QRFcapacity")
 # library(QRFcapacity)
 # data("rch_200")
 
-ww_rch_sf = rch_200 %>%
-  filter(UniqueID %in% ww_sum_sf$UniqueID) %>%
-  st_transform(ww_crs)
+# trim down 200m reach layer
+uww_rch_sf = rch_200 %>%
+  filter(UniqueID %in% uww_sum_sf$UniqueID) %>%
+  st_transform(uww_crs)
 
-ggplot(ww_rch_sf) + geom_sf()
+ggplot(uww_rch_sf) + geom_sf()
 
-# compare with the stream network provided by CTUIR
+# compare with the stream network provided by CTUIR, this is for entire Walla Walla
 ctuir_rch_sf = st_read(here("analysis/data/raw_data/stream_network/CTUIRStillwaterStreamNetwork.shp")) %>%
-  st_transform(ww_crs)
+  st_transform(uww_crs)
 
 ggplot(ctuir_rch_sf) + geom_sf()
 
 # read in UWW base referencing geodatabase
-ww_gdb_filepath = here("analysis/data/raw_data/stream_network/UWW_base_referencing.gdb")
-ww_gdb_layers = ogrListLayers(ww_gdb_filepath)
+uww_gdb_filepath = here("analysis/data/raw_data/stream_network/UWW_base_referencing.gdb")
+uww_gdb_layers = ogrListLayers(uww_gdb_filepath)
 
 # extract UWW_streams_24k layer from geodatabase
-ww_base_ref_streams_24k  = readOGR(dsn = ww_gdb_filepath,
+uww_base_ref_streams_24k  = readOGR(dsn = uww_gdb_filepath,
                                    layer = "UWW_streams_24k") %>%
   st_as_sf() %>%
-  st_transform(ww_crs)
+  st_transform(uww_crs)
 
-ggplot(ww_base_ref_streams_24k) + geom_sf()
-
+ggplot(uww_base_ref_streams_24k) + geom_sf()
 # Thankfully, the base reference streams and our rch_200 appear to be the same linear networks!!!
 
 #-----------------------------------------------------------------
 # save data for this repository
-save(ww_sum_sf,
-     ww_win_sf,
-     ww_redd_sf,
-     ww_huc_sf,
-     ww_rch_sf,
-     ww_base_ref_streams_24k,
-     file = "analysis/data/derived_data/qrf_extrapolations.rda")
+save(uww_sum_sf,
+     uww_win_sf,
+     uww_redd_sf,
+     file = "analysis/data/derived_data/uww_qrf_extrapolations.rda")
+
+save(uww_huc_sf,
+     uww_rch_sf,
+     file = "analysis/data/derived_data/uww_spatial.rda")
 
 #-----------------------------------------------------------------
 # save geopackages for use in QGIS
-st_write(ww_sum_sf,
-         dsn = "analysis/data/derived_data/ww_juv_sum_qrf.gpkg",
+st_write(uww_sum_sf,
+         dsn = "analysis/data/derived_data/uww_juv_sum_qrf.gpkg",
          append = F)
-st_write(ww_win_sf,
-         dsn = "analysis/data/derived_data/ww_juv_win_qrf.gpkg",
+st_write(uww_win_sf,
+         dsn = "analysis/data/derived_data/uww_juv_win_qrf.gpkg",
          append = F)
-st_write(ww_redd_sf,
-         dsn = "analysis/data/derived_data/ww_redd_qrf.gpkg",
+st_write(uww_redd_sf,
+         dsn = "analysis/data/derived_data/uww_redd_qrf.gpkg",
          append = F)
-st_write(ww_rch_sf,
+st_write(uww_rch_sf,
          dsn = "analysis/data/derived_data/ww_rch_200.gpkg",
          append = F)
-st_write(ww_huc_sf,
-         dsn = "analysis/data/derived_data/ww_huc_bndry.gpkg",
+st_write(uww_huc_sf,
+         dsn = "analysis/data/derived_data/uww_huc_bndry.gpkg",
          append = F)
-st_write(ww_base_ref_streams_24k,
-         dsn = "analysis/data/derived_data/ww_base_ref_streams.gpkg",
+st_write(uww_base_ref_streams_24k,
+         dsn = "analysis/data/derived_data/uww_base_ref_streams.gpkg",
          append = F)
 
 #-----------------------------------------------------------------
@@ -196,15 +198,15 @@ st_write(ww_base_ref_streams_24k,
 # pick a river color
 river_color = "lightskyblue1"
 
-ww_sum_sf %>%
+uww_sum_sf %>%
   ggplot() +
-  geom_sf(data = ww_huc_sf %>%
+  geom_sf(data = uww_huc_sf %>%
             st_union() %>%
             nngeo::st_remove_holes(),
           fill = NA,
           color = "gray50") +
   geom_sf(color = river_color) +
-  geom_sf(data = ww_sum_sf %>%
+  geom_sf(data = uww_sum_sf %>%
             filter(chnk), # only records in the StreamNet Chinook domain
           aes(color = chnk_per_m2),
           size = 1) +
