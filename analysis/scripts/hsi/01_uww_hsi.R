41022ad62b942465fee6cd6bc15c0956bbe92f6c
bridger-bertram <105084513+bridger-bertram@users.noreply.github.com>
2024-02-05 15:44:22 -0700

    spring winter edits

--- analysis/scripts/hsi/01_uww_hsi.R
+++ analysis/scripts/hsi/01_uww_hsi.R
@@ -272,34 +272,31 @@ for(scn in scenarios) {
 #####################################################
 
 # to loop over scenarios
-scenarios  = c("chnk_spw", "chnk_juv", "sthd_juv") # Change to "chnk_win" and "sthd_win"
+scenarios  = c("chnk_win", "sthd_win") # Change to "chnk_win" and "sthd_win"
 years      = c("2019", "2021") # Keep same
 
 for(scn in scenarios) {
   for(yr in years) {
 
-    # read in appropriate depth and velocity rasters
+  #  read in appropriate depth and velocity rasters
     if(yr == "2019") {
-      d_rast = raster(here("analysis/data/raw_data/hsi_d_v_tifs/Depth_2019.tif"))
-      v_rast = raster(here("analysis/data/raw_data/hsi_d_v_tifs/Velocity_2019.tif"))
+      d_rast = raster("S:/main/data/habitat/HSI/UWW_LowFlowHydraulicResults/winter_spring_hydraulics_tiff/Combined_2019lidar_Winter_depth.tif")
+      v_rast = raster(here("analysis/data/raw_data/hsi_d_v_tifs/Combined_2019lidar_Winter_velocity.tif"))
     }
     if(yr == "2021") {
-      d_rast = raster(here("analysis/data/raw_data/hsi_d_v_tifs/Depth_2021.tif"))
-      v_rast = raster(here("analysis/data/raw_data/hsi_d_v_tifs/Velocity_2021.tif"))
+      d_rast = raster(here("analysis/data/raw_data/hsi_d_v_tifs/Combined_2021lidar_Winter_depth.tif"))
+      v_rast = raster(here("analysis/data/raw_data/hsi_d_v_tifs/Combined_2021lidar_Winter_velocity.tif"))
     }
 
+
     # grab the appropriate suitability curves -- update to pull in winter curves
-    if(scn == "chnk_spw") {
-      d_curve = get("chnk_spw_d")
-      v_curve = get("chnk_spw_v")
+    if(scn == "chnk_win") {
+      d_curve = get("chnk_win_d")
+      v_curve = get("chnk_win_v")
     }
-    if(scn == "chnk_juv") {
-      d_curve = get("chnk_juv_d")
-      v_curve = get("chnk_juv_v")
-    }
-    if(scn == "sthd_juv") {
-      d_curve = get("sthd_juv_d")
-      v_curve = get("sthd_juv_v")
+    if(scn == "sthd_win") {
+      d_curve = get("sthd_win_d")
+      v_curve = get("sthd_win_v")
     }
 
     # calculate depth and velocity suitabilities
@@ -498,5 +495,219 @@ for(scn in scenarios) {
 #
 #
 #####################################################
+
+
+scenarios  = c("sthd_spw")
+years      = c("2019", "2021") # Keep same
+
+for(scn in scenarios) {
+  for(yr in years) {
+
+    #  read in appropriate depth and velocity rasters
+    if(yr == "2019") {
+      d_rast = raster("S:/main/data/habitat/HSI/UWW_LowFlowHydraulicResults/winter_spring_hydraulics_tiff/Combined_2019lidar_Spring_depth.tif")
+      v_rast = raster(here("analysis/data/raw_data/hsi_d_v_tifs/Combined_2019lidar_Winter_velocity.tif"))
+    }
+    if(yr == "2021") {
+      d_rast = raster(here("analysis/data/raw_data/hsi_d_v_tifs/Combined_2021lidar_Winter_depth.tif"))
+      v_rast = raster(here("analysis/data/raw_data/hsi_d_v_tifs/Combined_2021lidar_Winter_velocity.tif"))
+    }
+
+
+    # grab the appropriate suitability curves -- update to pull in winter curves
+    if(scn == "sthd_spw") {
+      d_curve = get("sthd_spw_d")
+      v_curve = get("sthd_spw_v")
+    }
+    # if(scn == "sthd_win") {
+    #   d_curve = get("sthd_win_d")
+    #   v_curve = get("sthd_win_v")
+    # }
+    # if(scn == "sthd_juv") {
+    #   d_curve = get("sthd_juv_d")
+    #   v_curve = get("sthd_juv_v")
+    # }
+
+    # calculate depth and velocity suitabilities
+    d_suit = calc(d_rast, d_curve)
+    v_suit = calc(v_rast, v_curve)
+
+    # write out depth and velocity suitability rasters
+    writeRaster(d_suit, paste0(here(), "/analysis/hsi_outputs/rasters/", scn, "_", yr, "_d_suit.tif"), overwrite = T)
+    writeRaster(v_suit, paste0(here(), "/analysis/hsi_outputs/rasters/", scn, "_", yr, "_v_suit.tif"), overwrite = T)
+
+    # function to calculate composite suitability using geometric mean of depth and velocity
+    comp_hsi <- function(r1, r2) {
+      y = (sqrt(r1*r2))
+      return(y)
+    }
+
+    # calculate geometric mean of d and v suitabilities and write composite raster
+    comp_suit <- overlay(d_suit, v_suit, fun = comp_hsi)
+    writeRaster(comp_suit, paste0(here(), "/analysis/hsi_outputs/rasters/", scn, "_", yr, "_comp_suit.tif"), overwrite = T)
+
+    # calculate metrics for each polygon including wetted area, WUA, and HHS; extract raster values within each polygon
+    # first, geomorphic reaches
+    for(geo in geo_names) {
+      sf_tmp = raster::extract(d_suit,
+                               get(as.character(geo)),
+                               fun = NULL,
+                               df = TRUE,
+                               na.rm = TRUE) %>%
+        drop_na() %>%
+        mutate(ID = replace(ID, ID == 1, as.character(geo)))
+      assign(paste0("geo_dsi_", geo), sf_tmp)
+      rm(sf_tmp)
+    }
+    for(geo in geo_names) {
+      sf_tmp = raster::extract(v_suit,
+                               get(as.character(geo)),
+                               fun = NULL,
+                               df = TRUE,
+                               na.rm = TRUE) %>%
+        drop_na() %>%
+        mutate(ID = replace(ID, ID == 1, as.character(geo)))
+      assign(paste0("geo_vsi_", geo), sf_tmp)
+      rm(sf_tmp)
+    }
+    for(geo in geo_names) {
+      sf_tmp = raster::extract(comp_suit,
+                               get(as.character(geo)),
+                               fun = NULL,
+                               df = TRUE,
+                               na.rm = TRUE) %>%
+        drop_na() %>%
+        mutate(ID = replace(ID, ID == 1, as.character(geo)))
+      assign(paste0("geo_csi_", geo), sf_tmp)
+      rm(sf_tmp)
+    }
+
+    # merge geomorphic reach HSI's back together
+    geo_dsi = bind_rows(lapply(ls(pattern = "^geo_dsi_"), function(x) get(x) )) %>%
+      rename("value"  = "layer") %>%
+      mutate("metric" = "depth")
+
+    geo_vsi = bind_rows(lapply(ls(pattern = "^geo_vsi_"), function(x) get(x) )) %>%
+      rename("value"  = "layer") %>%
+      mutate("metric" = "velocity")
+
+    geo_csi = bind_rows(lapply(ls(pattern = "^geo_csi_"), function(x) get(x) )) %>%
+      rename("value"  = "layer") %>%
+      mutate("metric" = "composite")
+
+    geo_values = bind_rows(geo_dsi, geo_vsi, geo_csi) %>%
+      mutate("scenario" = scn) %>%
+      mutate("year" = yr)
+
+    # write raw geomorphic reach results
+    write_rds(geo_values,
+              paste0(here(), "/analysis/hsi_outputs/raw_results/", scn, "_", yr, "_geo.rda"))
+
+    # now, river kilometers
+    for(rkm in rkm_names) {
+      sf_tmp = raster::extract(d_suit,
+                               get(as.character(rkm)),
+                               fun = NULL,
+                               df = TRUE,
+                               na.rm = TRUE) %>%
+        drop_na() %>%
+        mutate(ID = replace(ID, ID == 1, as.character(rkm)))
+      assign(paste0("rkm_dsi_", rkm), sf_tmp)
+      rm(sf_tmp)
+    }
+    for(rkm in rkm_names) {
+      sf_tmp = raster::extract(v_suit,
+                               get(as.character(rkm)),
+                               fun = NULL,
+                               df = TRUE,
+                               na.rm = TRUE) %>%
+        drop_na() %>%
+        mutate(ID = replace(ID, ID == 1, as.character(rkm)))
+      assign(paste0("rkm_vsi_", rkm), sf_tmp)
+      rm(sf_tmp)
+    }
+    for(rkm in rkm_names) {
+      sf_tmp = raster::extract(comp_suit,
+                               get(as.character(rkm)),
+                               fun = NULL,
+                               df = TRUE,
+                               na.rm = TRUE) %>%
+        drop_na() %>%
+        mutate(ID = replace(ID, ID == 1, as.character(rkm)))
+      assign(paste0("rkm_csi_", rkm), sf_tmp)
+      rm(sf_tmp)
+    }
+
+    # merge river kilometer HSI's back together
+    rkm_dsi = bind_rows(lapply(ls(pattern = "^rkm_dsi_"), function(x) get(x) )) %>%
+      rename("value"  = "layer") %>%
+      mutate("metric" = "depth")
+
+    rkm_vsi = bind_rows(lapply(ls(pattern = "^rkm_vsi_"), function(x) get(x) )) %>%
+      rename("value"  = "layer") %>%
+      mutate("metric" = "velocity")
+
+    rkm_csi = bind_rows(lapply(ls(pattern = "^rkm_csi_"), function(x) get(x) )) %>%
+      rename("value"  = "layer") %>%
+      mutate("metric" = "composite")
+
+    rkm_values = bind_rows(rkm_dsi, rkm_vsi, rkm_csi) %>%
+      mutate("scenario" = scn) %>%
+      mutate("year" = yr)
+
+    # write raw river kilometer results
+    write_rds(rkm_values,
+              paste0(here(), "/analysis/hsi_outputs/raw_results/", scn, "_", yr, "_rkm.rda"))
+
+    # get pixel area, UWW seems to be 1m x 1m
+    pix_area = prod(res(comp_suit))
+
+    # calculate HSI metrics for the run (geomorphic reaches)
+    geo_mets = geo_csi %>%
+      mutate(pix_area = pix_area) %>%
+      group_by(ID) %>%
+      summarise(area_m2 = sum(pix_area),
+                WUA = sum(value),
+                HHS = WUA/area_m2) %>%
+      left_join(geo_dsi %>%
+                  mutate(pix_area = pix_area) %>%
+                  group_by(ID) %>%
+                  summarise(d_HSI = sum(value) / sum(pix_area))) %>%
+      left_join(geo_vsi %>%
+                  mutate(pix_area = pix_area) %>%
+                  group_by(ID) %>%
+                  summarise(v_HSI = sum(value) / sum(pix_area))) %>%
+      mutate(scenario = scn,
+             year = yr)
+
+    # write out geomorphic reach results to .csv
+    write_csv(geo_mets, paste0(here(), "/analysis/hsi_outputs/csvs/", scn, "_", yr, "_geo_results.csv"))
+
+    # calculate HSI metrics for the run (river kilometers)
+    rkm_mets = rkm_csi %>%
+      mutate(pix_area = pix_area) %>%
+      group_by(ID) %>%
+      summarise(area_m2 = sum(pix_area),
+                WUA = sum(value),
+                HHS = WUA/area_m2) %>%
+      left_join(rkm_dsi %>%
+                  mutate(pix_area = pix_area) %>%
+                  group_by(ID) %>%
+                  summarise(d_HSI = sum(value) / sum(pix_area))) %>%
+      left_join(rkm_vsi %>%
+                  mutate(pix_area = pix_area) %>%
+                  group_by(ID) %>%
+                  summarise(v_HSI = sum(value) / sum(pix_area))) %>%
+      mutate(scenario = scn,
+             year = yr)
+
+    # write out geomorphic reach results to .csv
+    write_csv(rkm_mets, paste0(here(), "/analysis/hsi_outputs/csvs/", scn, "_", yr, "_rkm_results.csv"))
+
+    # print message for each loop
+    message(paste0("Run ", scn, " ", yr, " is complete."))
+
+  } # end years loop
+}   # end scenarios loop
 
 
